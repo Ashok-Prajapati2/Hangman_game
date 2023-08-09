ea74e0b95047fa6f8c845f5d3d69288a806a4185
ashok <ak4132592ashok@gmail.com>
2023-08-09 17:52:42 +0530

    base_complate

--- hangman.py
+++ hangman.py
@@ -4,12 +4,44 @@
 #########################################################
 
 import random
-import word
+from word import word_list
 import img 
 
 print(img.welcome + img.logo)
-hung_man = [1,2,3,4,5,6]
 
-word_list = word.word_list
 stages = img.stages
-print(stages)
+
+word_chouse = random.choice(word_list)
+print(word_chouse)
+
+ch_list = []
+for n in range(len(word_chouse)):
+    ch_list += "_"
+print(ch_list)    
+
+game_is_end = False
+while not game_is_end:
+    user_input = (input("ENTER  LETTER : ")).lower()
+    # print(user_input)
+    
+    for word in range(len(word_chouse)):
+        latter = word_chouse[word]
+        # print(f"{word} {word_chouse} {latter}")
+    
+        if latter == user_input:
+            ch_list[word] = user_input
+    
+        else:
+            pass
+            # print("wrong") 
+    if not "_" in ch_list:
+        game_is_end= True
+        print("Game Over!")
+        
+    
+    
+    f_word = ""
+    for n in ch_list:
+         f_word += n
+    
+    print(f_word)     
