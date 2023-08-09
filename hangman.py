d1e0267eb6c61e9c6a7e1c0b3115e3ae4e60499c
ashok <ak4132592ashok@gmail.com>
2023-08-09 21:16:25 +0530

    game_over

--- hangman.py
+++ hangman.py
@@ -11,11 +11,11 @@ print(img.welcome + img.logo)
 
 stages = img.stages
 
-word_chouse = random.choice(word_list)
-print(word_chouse)
+word_choose = random.choice(word_list)
+# print(word_choose)
 
 ch_list = []
-for n in range(len(word_chouse)):
+for n in range(len(word_choose)):
     ch_list += "_"
 print(ch_list)    
 
@@ -24,34 +24,31 @@ game_is_end = False
 lives = 6
 
 while not game_is_end:
-    user_input = (input("ENTER  LETTER : ")).lower()
+    user_input = (input("\n ENTER  LETTER : ")).lower()
     # print(user_input)
-
-    for word in range(len(word_chouse)):
-        latter = word_chouse[word]
-        # print(f"{word} {word_chouse} {latter}")
+    
+    if user_input in word_choose:
         
-        if latter == user_input:
-            ch_list[word] = user_input
-         
-        else:
-            pass
-    if latter not in user_input :
+        for word in range(len(word_choose)):
+        # print(f"{word} {word_chouse} {latter}")
+            if word_choose[word] == user_input:
+               ch_list[word] = user_input
+        
+    else:
         lives -= 1  
         print(f"your {lives} attend remainig") 
-        print(stages[-lives-1])  
+        print(stages[-lives-1]) 
         if lives == 0:
             game_is_end= True
-            print("You lose!")
+            print(f"You lose! \n Right word is : {word_choose}")
         
     if not "_" in ch_list:
         game_is_end= True
+        print(" You win ")
         print("Game Over!")
         
     
     
-    f_word = ""
-    for n in ch_list:
-         f_word += n
-    
-    print(f"Your progress {f_word}")     
+  
+    print(f"Your progress: {' '.join(ch_list)}")
+        
